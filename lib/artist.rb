719a85596380ceb73e341d845245acf73ad230f4
Jack Madden <jck13mad@gmail.com>
2020-03-30 22:44:52 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,24 @@
+class Artist 
+  
+  attr_accessor :name, :songs 
+  
+  @@all = []
+  
+  def initialize
+    @name = name
+    @songs = []
+  end
+  
+  def add_song(song)
+    @songs << song
+  end
+  
+  def songs
+   @songs
+  end
+  
+  def print_songs
+    
+  end
+  
+end
